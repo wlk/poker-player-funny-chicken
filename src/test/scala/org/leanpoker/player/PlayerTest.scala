50dc475f5ae8a4ad53c0bff7d7d6bcc44b664b36
Wojciech Langiewicz <wlangiewicz@gmail.com>
2016-03-06 11:01:56 +0100

    java to scala list conversions

--- src/test/scala/org/leanpoker/player/PlayerTest.scala
+++ src/test/scala/org/leanpoker/player/PlayerTest.scala
@@ -2,6 +2,7 @@ package org.leanpoker.player
 
 import com.google.gson.JsonParser
 import org.scalatest.{FunSpec, MustMatchers}
+import scala.collection.JavaConverters._
 
 class PlayerTest extends FunSpec with MustMatchers {
 
@@ -20,7 +21,7 @@ class PlayerTest extends FunSpec with MustMatchers {
     val jsonElement = new JsonParser()
       .parse("{\n  \"players\":[\n    {\n      \"name\":\"Player 1\",\n      \"stack\":1000,\n      \"status\":\"active\",\n      \"bet\":0,\n      \"hole_cards\":[],\n      \"version\":\"Version name 1\",\n      \"id\":0\n    },\n    {\n      \"name\":\"Player 2\",\n      \"stack\":1000,\n      \"status\":\"active\",\n      \"bet\":0,\n      \"hole_cards\":[],\n      \"version\":\"Version name 2\",\n      \"id\":1\n    }\n  ],\n  \"tournament_id\":\"550d1d68cd7bd10003000003\",\n  \"game_id\":\"550da1cb2d909006e90004b1\",\n  \"round\":0,\n  \"bet_index\":0,\n  \"small_blind\":10,\n  \"orbits\":0,\n  \"dealer\":0,\n  \"community_cards\":[],\n  \"current_buy_in\":0,\n  \"pot\":0\n}")
     Player.betRequest(jsonElement)
-    Player.game.players.get(0).stack must be(1000)
+    Player.game.players.asScala.head.stack must be(1000)
   }
 
 }
