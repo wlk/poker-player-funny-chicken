f5d92c297ac5a927a76fbe6443d877cc40ed15a1
Wojciech Langiewicz <wlangiewicz@gmail.com>
2016-03-06 11:00:38 +0100

    parsing ok

--- src/main/scala/org/leanpoker/player/Player.scala
+++ src/main/scala/org/leanpoker/player/Player.scala
@@ -1,14 +1,16 @@
 package org.leanpoker.player
 
 import com.google.gson.{Gson, JsonElement}
+import scala.collection.JavaConverters._
+import java.{util => ju}
 
 object Player {
-
   val VERSION = "Funny Chicken"
+  var game: Game = _
+  val gson = new Gson
 
   def betRequest(request: JsonElement) = {
-    /*val gson = new Gson
-    val game = gson.fromJson(request, classOf[GameState])*/
+    game = gson.fromJson(request, classOf[Game])
     20
   }
 
@@ -19,6 +21,6 @@ object Player {
 
 case class Card(rank: String, suit: String)
 
-case class Player(name: String, stack: Int, status: String, bet: Int, hole_cards: List[Card], version: String, id: Int)
+case class Player(name: String, stack: Int, status: String, bet: Int, hole_cards: ju.List[Card], version: String, id: Int)
 
-case class GameState(players: List[Player], tournament_id: String, game_id: String, round: Int, bet_index: Int, small_blind: Int, orbits: Int, dealer: Int, community_cards: List[Card], current_buy_in: Int, pot: Int)
+case class Game(players: ju.List[Player], tournament_id: String, game_id: String, round: Int, bet_index: Int, small_blind: Int, orbits: Int, dealer: Int, community_cards: ju.List[Card], current_buy_in: Int, pot: Int)
--- src/test/scala/org/leanpoker/player/PlayerTest.scala
+++ src/test/scala/org/leanpoker/player/PlayerTest.scala
@@ -16,4 +16,11 @@ class PlayerTest extends FunSpec with MustMatchers {
     Player.betRequest(jsonElement) must be(20)
   }
 
+  it("should parse request - 2") {
+    val jsonElement = new JsonParser()
+      .parse("{\n  \"players\":[\n    {\n      \"name\":\"Player 1\",\n      \"stack\":1000,\n      \"status\":\"active\",\n      \"bet\":0,\n      \"hole_cards\":[],\n      \"version\":\"Version name 1\",\n      \"id\":0\n    },\n    {\n      \"name\":\"Player 2\",\n      \"stack\":1000,\n      \"status\":\"active\",\n      \"bet\":0,\n      \"hole_cards\":[],\n      \"version\":\"Version name 2\",\n      \"id\":1\n    }\n  ],\n  \"tournament_id\":\"550d1d68cd7bd10003000003\",\n  \"game_id\":\"550da1cb2d909006e90004b1\",\n  \"round\":0,\n  \"bet_index\":0,\n  \"small_blind\":10,\n  \"orbits\":0,\n  \"dealer\":0,\n  \"community_cards\":[],\n  \"current_buy_in\":0,\n  \"pot\":0\n}")
+    Player.betRequest(jsonElement)
+    Player.game.players.get(0).stack must be(1000)
+  }
+
 }
