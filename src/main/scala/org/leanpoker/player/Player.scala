ce299fa8b73cf3ae42da4ad0f7c54b6bf14e048a
Jakub Przychodzen <przychodzen.j@gmail.com>
2016-03-06 14:37:25 +0100

    fix issue

--- src/main/scala/org/leanpoker/player/Player.scala
+++ src/main/scala/org/leanpoker/player/Player.scala
@@ -4,7 +4,7 @@ import com.google.gson.{Gson, JsonElement}
 import java.{util => ju}
 
 object Player {
-  val VERSION = "Funny Chicken 15"
+  val VERSION = "Funny Chicken 16"
 
   var game: Game = _
 
