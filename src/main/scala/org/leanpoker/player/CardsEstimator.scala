d8d75baa0f203b120f39736f6bec7b2a1d9f3148
Wojciech Langiewicz <wlangiewicz@gmail.com>
2016-03-06 11:56:51 +0100

    fix to card estimator

--- src/main/scala/org/leanpoker/player/CardsEstimator.scala
+++ src/main/scala/org/leanpoker/player/CardsEstimator.scala
@@ -2,24 +2,24 @@ package org.leanpoker.player
 
 object CardsEstimator {
   def estimate(l: List[Card]): Double = {
-    var good = 20.0
+    var good = 0.2
     for {
       c1 <- l
       c2 <- l
     } yield {
       if (c1.rank.equals(c2.rank)) {
-        good = 90.0
+        good = 0.9
         return good
       } else {
         c1.rank match {
-          case "A" => good += 5
-          case "K" => good += 4
-          case "Q" => good += 3
-          case "J" => good += 2
-          case "10" => good += 1
+          case "A" => good += 0.05
+          case "K" => good += 0.04
+          case "Q" => good += 0.03
+          case "J" => good += 0.02
+          case "10" => good += 0.01
         }
       }
     }
-    good / 100.0
+    good
   }
 }
