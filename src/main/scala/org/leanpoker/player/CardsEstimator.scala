2710a039dd05fd6188767f42059cedb6b7fa54e3
Jakub Przychodzen <przychodzen.j@gmail.com>
2016-03-06 12:52:18 +0100

    pair deteciton

--- src/main/scala/org/leanpoker/player/CardsEstimator.scala
+++ src/main/scala/org/leanpoker/player/CardsEstimator.scala
@@ -82,6 +82,9 @@ object CardsEstimator {
   def handPoints(l: List[Card]): Double = {
     val card1 = l.head
     val card2 = l(1)
+    if(card1.rank.equals(card2.rank))
+      0.2
+    else
     getPointsFromCard(card1) + getPointsFromCard(card2) + howFar(card1, card2)
   }
 }
