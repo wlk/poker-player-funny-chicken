0671e61262f5db80f2ec4f553dd19fb0184690b1
Wojciech Langiewicz <wlangiewicz@gmail.com>
2016-03-06 13:38:05 +0100

    refactoring, introduced game engine

--- src/main/scala/org/leanpoker/player/Player.scala
+++ src/main/scala/org/leanpoker/player/Player.scala
@@ -1,7 +1,6 @@
 package org.leanpoker.player
 
 import com.google.gson.{Gson, JsonElement}
-import scala.collection.JavaConverters._
 import java.{util => ju}
 
 object Player {
@@ -20,57 +19,9 @@ object Player {
   }
 
   def requestGame(game: Game) = {
-    val shouldRaise = true
-    if (shouldRaise) {
-      rasingGame(game)
-    } else {
-      25
-    }
-
+    val engine = new GameEngine(game)
+    engine.processGame
   }
-
-  def myCards = {
-    game.players.asScala(game.in_action).hole_cards.asScala.toList
-  }
-
-  def isAllIn = {
-    minimumRaise >= game.players.asScala(game.in_action).stack
-  }
-
-  def minimumRaise = {
-    def raise(current_buy_in: Int, my_bet: Int, minimum_raise: Int): Int = {
-      current_buy_in - my_bet + minimum_raise
-    }
-    raise(game.current_buy_in, game.players.asScala(game.in_action).bet, game.minimum_raise)
-  }
-
-
-  def rasingGame(game: Game): Int = {
-    val estimation = CardsEstimator.estimateWithHand(myCards, game.community_cards.asScala.toList)
-
-    if (estimation >= 0.8) {
-      // good cards
-      if (minimumRaise >= game.players.asScala(game.in_action).stack) {
-        // all in
-        minimumRaise
-      } else {
-        (minimumRaise * (1 + estimation)).toInt
-      }
-    } else {
-      if (estimation < 0.3) {
-        // bad cards
-        0
-      } else {
-        // ok cards
-        if (isAllIn) {
-          0
-        } else {
-          minimumRaise
-        }
-      }
-    }
-  }
-
 
   def showdown(game: JsonElement) {
 
