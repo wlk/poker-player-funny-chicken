aef5afa49f0b2bc5daa22769cbfef2aee133f0fa
Jakub Przychodzen <przychodzen.j@gmail.com>
2016-03-06 11:21:04 +0100

    Z

--- src/main/scala/org/leanpoker/player/CardsParser.scala
+++ src/main/scala/org/leanpoker/player/CardsParser.scala
@@ -6,3 +6,24 @@ package org.leanpoker.player
 class CardsParser {
   
 }
+
+clubs,spades,hearts,diamonds
+2-10 and J,Q,K,A
+
+
+def listOfPossibile(l : List[Card]) : Double {
+  var good = 20.0
+  for(Card c : l) {
+	 for(Card c1 : l) {
+	  if (c1.rank.equals(c2.rank))
+		80.0
+  	}
+	c.rank match {
+		case "A" => good += 5
+		case "K" => good += 4
+		case "Q" => good += 3
+		case "J" => good += 2
+		case "10" => good += 1
+	}
+  }
+}	
