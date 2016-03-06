85f3d204dba10bcd56fee0c8bdfc51e8bd471838
Wojciech Langiewicz <wlangiewicz@gmail.com>
2016-03-06 19:00:19 +0100

    version bump

--- src/main/scala/org/leanpoker/player/Player.scala
+++ src/main/scala/org/leanpoker/player/Player.scala
@@ -4,7 +4,7 @@ import com.google.gson.{Gson, JsonElement}
 import java.{util => ju}
 
 object Player {
-  val VERSION = "Funny Chicken 26"
+  val VERSION = "Funny Chicken 27"
 
   var game: Game = _
 
